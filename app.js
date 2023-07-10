29c045f3d2d9ccaa650e68dbe7420b5a26becce4
kutaymalik <56682209+kutaymalik@users.noreply.github.com>
2023-07-10 15:26:43 +0300

    navigation setting

--- app.js
+++ app.js
@@ -2,11 +2,25 @@ import express from 'express';
 
 const app = express();
 
+// Template Engine
+app.set('view engine', 'ejs');
+
+// MIDDLEWARES
+app.use(express.static("public"));
+
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.status(200).send('index page');
+    res.status(200).render('index', {
+        pageName: 'index'
+    });
 });
+
+app.get('/about', (req, res) => {
+    res.status(200).render('about', {
+        pageName: 'about'
+    });
+})
 
 app.listen(port, () => {
     console.log(`App started on ${port} port`);
